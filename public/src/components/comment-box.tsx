20b37c773893cfd171883493efa4f3cbfb63b422
Nathan Kerr <nathankerr@gmail.com>
2016-09-09 11:29:39 -0700

    Fixes to make babel happy.

--- public/src/components/comment-box.tsx
+++ public/src/components/comment-box.tsx
@@ -4,96 +4,105 @@ import CommentList from './comment-list';
 import CommentForm from './comment-form';
 
 interface CommentDataItem {
-  id: number
-  author: string
-  text: string
+    id: number
+    author: string
+    text: string
 }
 interface CommentBoxProps {
-  url: string
-  pollInterval: number
+    url: string
+    pollInterval: number
 }
 interface CommentBoxState {
-  data: CommentDataItem[]
+    data: CommentDataItem[]
 }
 
 export default class CommentBox extends React.Component<CommentBoxProps, CommentBoxState> {
-  constructor(props) {
-    super(props);
-    this.state = {data: []};
-  }
-  handleErrors(response) {
-    if (!response.ok) {
-      return response.json().then(function(err) {
-        throw Error(err.message);
-      });
-    } else {
-      return response;
+    constructor(props) {
+        super(props);
+        this.state = {data: []};
     }
-  }
-  prepare(response) {
-    console.info('ok?: ', response.ok);
-    console.info('status: ', response.status);
-    console.info('statusText: ', response.statusText);
-    return response.json();
-  }
-  onFulfilled(data) {
-    let message = ([
-      '成功ハンドラで処理されました。',
-      'data: ' + JSON.stringify(data, null, '  '),
-    ]).join('\n');
-    console.log(message);
-    this.setState({data: data});
-  }
-  onRejected(err) {
-    var message = ([
-      '失敗ハンドラで処理されました。',
-      'error: ' + err.message,
-    ]).join('\n');
-    console.error(err);
-    console.log(message);
-  }
-  onRejectedPOST(err) {
-    this.setState({data: this.state.data});
-    this.onRejected(err);
-  }
-  public loadCommentsFromServer() {
-    // cf. http://fetch-api-sample.azurewebsites.net/demo03.html
-    fetch(this.props.url)
-      .then(this.handleErrors.bind(this))
-      .then(this.prepare.bind(this))
-      .then(this.onFulfilled.bind(this))
-      .catch(this.onRejected.bind(this));
-  }
-  public handleCommentSubmit(comment) {
-    let comments = this.state.data;
-    comment.id = Date.now();
-    let newComments = comments.concat([comment]);
-    this.setState({data: newComments});
 
-    fetch(this.props.url, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(comment)
-    })
-    .then(this.handleErrors.bind(this))
-    .then(this.prepare.bind(this))
-    .then(this.onFulfilled.bind(this))
-    .catch(this.onRejectedPOST.bind(this));
-  }
-  public componentDidMount() {
-    this.loadCommentsFromServer();
-    setInterval(this.loadCommentsFromServer.bind(this), this.props.pollInterval);
-  }
-  public render() {
-    return (
-      <div className="commentBox">
-        <h1>Comments</h1>
-        <CommentList data={this.state.data} />
-        <CommentForm onCommentSubmit={this.handleCommentSubmit.bind(this)} />
-      </div>
-    );
-  }
+    handleErrors(response) {
+        if (!response.ok) {
+            return response.json().then(function (err) {
+                throw Error(err.message);
+            });
+        } else {
+            return response;
+        }
+    }
+
+    prepare(response) {
+        console.info('ok?: ', response.ok);
+        console.info('status: ', response.status);
+        console.info('statusText: ', response.statusText);
+        return response.json();
+    }
+
+    onFulfilled(data) {
+        let message = ([
+            '成功ハンドラで処理されました。',
+            'data: ' + JSON.stringify(data, null, '  '),
+        ]).join('\n');
+        console.log(message);
+        this.setState({data: data});
+    }
+
+    onRejected(err) {
+        var message = ([
+            '失敗ハンドラで処理されました。',
+            'error: ' + err.message,
+        ]).join('\n');
+        console.error(err);
+        console.log(message);
+    }
+
+    onRejectedPOST(err) {
+        this.setState({data: this.state.data});
+        this.onRejected(err);
+    }
+
+    public loadCommentsFromServer() {
+        // cf. http://fetch-api-sample.azurewebsites.net/demo03.html
+        fetch(this.props.url)
+            .then(this.handleErrors.bind(this))
+            .then(this.prepare.bind(this))
+            .then(this.onFulfilled.bind(this))
+            .catch(this.onRejected.bind(this));
+    }
+
+    public handleCommentSubmit(comment) {
+        let comments = this.state.data;
+        comment.id = Date.now();
+        let newComments = comments.concat([comment]);
+        this.setState({data: newComments});
+
+        fetch(this.props.url, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(comment)
+        })
+            .then(this.handleErrors.bind(this))
+            .then(this.prepare.bind(this))
+            .then(this.onFulfilled.bind(this))
+            .catch(this.onRejectedPOST.bind(this));
+    }
+
+    public componentDidMount() {
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer.bind(this), this.props.pollInterval);
+    }
+
+    public render() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.state.data}/>
+                <CommentForm onCommentSubmit={this.handleCommentSubmit.bind(this)}/>
+            </div>
+        );
+    }
 }
--- rollup.config.js
+++ rollup.config.js
@@ -2,28 +2,31 @@ import typescript from 'rollup-plugin-typescript';
 import babel   from 'rollup-plugin-babel'
 import replace from 'rollup-plugin-replace'
 import uglify from 'rollup-plugin-uglify';
+import eslint from 'rollup-plugin-eslint';
 
 export default {
-  entry: './public/src/index.tsx',
-  dest: './public/dist/app.min.js',
-  format: 'iife',
-  sourceMap: './public/dist/app.min.js.map',
-  moduleName: 'MyBundle',
-  plugins: [
-    typescript(),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    babel(),
-    uglify()
-  ],
-  globals: {
-    'react': 'React',
-    'react-dom':'ReactDOM',
-    'redux':'Redux',
-    'react-redux':'reactRedux',
-    'redux-thunk':'ReduxThunk',
-    'redux-logger':'redux-logger',
-    'jquery':'jQuery'
-  }
+	entry: './public/src/index.tsx',
+	dest: './public/dist/app.min.js',
+	format: 'iife',
+	sourceMap: './public/dist/app.min.js.map',
+	moduleName: 'MyBundle',
+	plugins: [
+		typescript(),
+
+		replace({
+			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+		}),
+		babel(),
+		//eslint(), //Commenting out for now. Need to figure out how to get it to lay off the "__" vars, possibly switch to tslint
+		(process.env.NODE_ENV === 'production' && uglify())
+	],
+	globals: {
+		'react': 'React',
+		'react-dom': 'ReactDOM',
+		'redux': 'Redux',
+		'react-redux': 'reactRedux',
+		'redux-thunk': 'ReduxThunk',
+		'redux-logger': 'redux-logger',
+		'jquery': 'jQuery'
+	}
 }
